5d5ce83063fd76bfe63b330dbafb317bdf326e3d
Syrnnik <sakh.alexandr@gmail.com>
2024-06-19 00:16:50 +0300

    feat: get all users & delete user reqs

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,3 +5,12 @@ export const getCurrentUser = async (): Promise<User> => {
   const { data: user } = await axiosClient.get("/user/current/")
   return user
 }
+
+export const getAllUsers = async (): Promise<User[]> => {
+  const { data: usersList } = await axiosClient.get("/user/")
+  return usersList
+}
+
+export const deleteUser = async (userId: number) => {
+  await axiosClient.delete(`/user/${userId}`)
+}
