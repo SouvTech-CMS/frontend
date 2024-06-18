162c50e127ddb254e2ca7e0b01a2f1859149b64e
Syrnnik <sakh.alexandr@gmail.com>
2024-06-19 00:17:46 +0300

    fix: user type & type for creating user

--- src/type/user.ts
+++ src/type/user.ts
@@ -1,14 +1,27 @@
 import { Permission } from "type/permission"
 import { Role } from "type/role"
+import { Shop } from "type/shop"
 
 export type User = {
-  id: number
+  id?: number
   username: string
-  salary: string
-  bot_user_id: number
-  e_mail: string
-  phone_number: string
+  password?: string
+  fio?: string
+  salary?: number
+  bot_user_id?: number
+  email?: string
+  phone?: string
   roles_with_permissions: RoleWithPermissions[]
+  shops: Shop[]
+}
+
+export type UserCreate = {
+  username: string
+  password?: string
+  fio: string
+  salary: number
+  email: string
+  phone: string
 }
 
 export type RoleWithPermissions = {
