760ff9b41cd9f56d53a1b715a502446ed5a08432
Syrnnik <sakh.alexandr@gmail.com>
2024-06-19 06:52:57 +0300

    feat: create and update user reqs

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,14 +1,22 @@
 import { axiosClient } from "api/axiosClient"
-import { User } from "type/user"
+import { UserCreateOrUpdate, UserWithRolesAndShops } from "type/user"
 
-export const getCurrentUser = async (): Promise<User> => {
+export const getCurrentUser = async (): Promise<UserWithRolesAndShops> => {
   const { data: user } = await axiosClient.get("/user/current/")
   return user
 }
 
-export const getAllUsers = async (): Promise<User[]> => {
+export const getAllUsers = async (): Promise<UserWithRolesAndShops[]> => {
   const { data: usersList } = await axiosClient.get("/user/")
   return usersList
+}
+
+export const createUser = async (user: UserCreateOrUpdate) => {
+  await axiosClient.post("/user/", user)
+}
+
+export const updateUser = async (user: UserCreateOrUpdate) => {
+  await axiosClient.put("/user/", user)
 }
 
 export const deleteUser = async (userId: number) => {
