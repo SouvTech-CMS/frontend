567fb34f3313d73f150bd37beea5de3c5f535aba
Syrnnik <sakh.alexandr@gmail.com>
2024-06-19 06:53:16 +0300

    refactor: move validations to utils

---
+++ src/util/validation.ts
@@ -0,0 +1,22 @@
+// const EMAIL_REGEX = /^\S+@\S+\.\S+$/
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
+
+export const isUsernameValid = (username: string) => {
+  if (username.length < 5) {
+    return false
+  }
+
+  return true
+}
+
+export const isPasswordValid = (password: string) => {
+  if (password.length < 8) {
+    return false
+  }
+
+  if (!PASSWORD_REGEX.test(password)) {
+    return false
+  }
+
+  return true
+}
