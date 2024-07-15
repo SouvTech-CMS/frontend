d7ab921f3a04f326055571df2f5a860c1711c668
Syrnnik <sakh.alexandr@gmail.com>
2024-07-15 05:56:44 +0300

    fix: shelfs input

--- src/component/storageGood/ShelfBadge.tsx
+++ src/component/storageGood/ShelfBadge.tsx
@@ -1,12 +1,23 @@
-import { Badge } from "@chakra-ui/react"
+import { Badge, Tag, TagCloseButton, TagLabel } from "@chakra-ui/react"
 import { FC } from "react"
 
 interface ShelfBadgeProps {
   shelf: string
+  onRemove?: () => void
+  isCanRemove?: boolean
 }
 
 export const ShelfBadge: FC<ShelfBadgeProps> = (props) => {
-  const { shelf } = props
+  const { shelf, onRemove, isCanRemove } = props
+
+  if (isCanRemove && onRemove) {
+    return (
+      <Tag size="md" fontWeight="bold" colorScheme="purple">
+        <TagLabel>{shelf}</TagLabel>
+        <TagCloseButton onClick={onRemove} />
+      </Tag>
+    )
+  }
 
   return (
     <Badge fontSize="sm" colorScheme="purple">
