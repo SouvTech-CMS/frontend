40c1265d652ffd7965d24c6c1134f906e5f86d9d
Syrnnik <sakh.alexandr@gmail.com>
2024-08-25 01:30:30 +0300

    feat: user permissions hook

---
+++ src/constant/permissions.ts
@@ -0,0 +1,22 @@
+export enum Permission {
+  //* Reports
+  REPORT_DETAILED = "report_detailed",
+  REPORT_GENERAL = "report_general",
+
+  //* Documents
+  DOCUMENT_READ = "document_read",
+  DOCUMENT_WRITE = "document_write",
+  DOCUMENT_DELETE = "document_delete",
+
+  //* Storage
+  STORAGE_READ = "storage_read",
+  STORAGE_WRITE = "storage_write",
+
+  //* Supplier
+  SUPPLIER_READ = "supplier_read",
+  SUPPLIER_WRITE = "supplier_write",
+
+  //* Pruchase
+  PURCHASE_READ = "purchase_read",
+  PURCHASE_WRITE = "purchase_write",
+}
