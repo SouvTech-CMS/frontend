ab39e1b283b1e76ab41f69534045f2fd346abf64
Syrnnik <sakh.alexandr@gmail.com>
2024-11-18 21:41:16 +0300

    feat: notify when api error on create or update storage good

--- src/service/storage/storageGood.ts
+++ src/service/storage/storageGood.ts
@@ -1,11 +1,20 @@
 import { queryClient } from "api/queryClient"
 import { createStorageGood, updateStorageGood } from "api/storage/storageGood"
+import { AxiosError } from "axios"
 import { useMutation } from "react-query"
+import { notify } from "util/toasts"
 
 export const useStorageGoodCreateMutation = () => {
   return useMutation(createStorageGood, {
     onSuccess: () => {
       queryClient.invalidateQueries("storageGoodsList")
+    },
+    onError: (error: AxiosError) => {
+      const responseData = error.response?.data as { detail?: string }
+
+      if (responseData?.detail) {
+        notify(responseData.detail, "error")
+      }
     },
   })
 }
@@ -14,6 +23,13 @@ export const useStorageGoodUpdateMutation = () => {
   return useMutation(updateStorageGood, {
     onSuccess: () => {
       queryClient.invalidateQueries("storageGoodsList")
+    },
+    onError: (error: AxiosError) => {
+      const responseData = error.response?.data as { detail?: string }
+
+      if (responseData?.detail) {
+        notify(responseData.detail, "error")
+      }
     },
   })
 }
