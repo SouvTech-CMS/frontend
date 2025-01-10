8a041dec7ba4160efa860d03ac30dea6a164c0b2
Syrnnik <sakh.alexandr@gmail.com>
2025-01-10 06:09:44 +0300

    feat: defects cards on storage good details page & collapsible cards grid

--- src/api/engraver/engraver.ts
+++ src/api/engraver/engraver.ts
@@ -6,3 +6,10 @@ export const getAllEngravers = async (): Promise<WithId<Engraver>[]> => {
   const { data: engraversList } = await axiosClient.get("/engraver/all/")
   return engraversList
 }
+
+export const getEngraverById = async (
+  engraverId: number,
+): Promise<WithId<Engraver>> => {
+  const { data: engraver } = await axiosClient.get(`/engraver/id/${engraverId}`)
+  return engraver
+}
