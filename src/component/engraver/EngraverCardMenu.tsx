e99d7f0fcf83e16c3314a22a50995e83d424bf76
Syrnnik <sakh.alexandr@gmail.com>
2025-01-10 12:46:07 +0300

    feat: engraver card

---
+++ src/component/engraver/EngraverCardMenu.tsx
@@ -0,0 +1,42 @@
+import {
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react"
+import { FC } from "react"
+import { FiEdit, FiMoreVertical, FiTrash2 } from "react-icons/fi"
+
+interface EngraverCardMenuProps {
+  onEdit: () => void
+  onBlock: () => void
+}
+
+export const EngraverCardMenu: FC<EngraverCardMenuProps> = (props) => {
+  const { onEdit, onBlock } = props
+
+  return (
+    <Flex position="absolute" top={0} right={0}>
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="engraver-card-menu"
+          icon={<FiMoreVertical />}
+          variant="ghost"
+        />
+
+        <MenuList>
+          <MenuItem icon={<FiEdit />} onClick={onEdit}>
+            Edit
+          </MenuItem>
+
+          <MenuItem icon={<FiTrash2 />} color="red" onClick={onBlock}>
+            Block
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex>
+  )
+}
