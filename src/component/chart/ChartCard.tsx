fde2b2a9d23b27cc5b05ac683a105a7ece37aa9f
Syrnnik <sakh.alexandr@gmail.com>
2025-01-15 00:25:51 +0300

    refactor: chard card

--- src/component/chart/ChartCard.tsx
+++ src/component/chart/ChartCard.tsx
@@ -1,4 +1,13 @@
-import { Card, CardBody, ChakraProps, Flex, Icon, Text } from "@chakra-ui/react"
+import {
+  ChakraProps,
+  Flex,
+  Icon,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react"
 import { FC } from "react"
 import { IconType } from "react-icons"
 
@@ -14,40 +23,46 @@ export const ChartCard: FC<ChartCardProps> = (props) => {
   const { icon, color = "blue", title, value, isIconHidden } = props
 
   return (
-    <Card>
-      <CardBody px={4} py={3} pr={20}>
+    <Flex
+      bgColor="white"
+      direction="row"
+      alignItems="center"
+      py={3}
+      pb={1}
+      px={5}
+      pr={20}
+      borderRadius={10}
+      gap={5}
+    >
+      {/* Big Icon */}
+      {!isIconHidden && (
         <Flex
-          h="full"
-          direction="row"
-          justifyContent="flex-start"
+          bgColor={`${color}.100`}
+          justifyContent="center"
           alignItems="center"
-          gap={5}
+          p={4}
+          mb={2}
+          borderRadius="50%"
         >
-          {/* Big Icon */}
-          {!isIconHidden && (
-            <Flex
-              h="full"
-              alignItems="center"
-              bgColor={`${color}.100`}
-              p={3}
-              borderRadius="50%"
-            >
-              <Icon as={icon} boxSize="full" color={color} />
-            </Flex>
-          )}
-
-          {/* Title & Value */}
-          <Flex direction="column" justifyContent="center">
-            <Text fontSize="sm" color="gray" fontWeight="light">
-              {title}
-            </Text>
-
-            <Text fontSize="xl" fontWeight="bold">
-              {value}
-            </Text>
-          </Flex>
+          <Icon as={icon} fontSize="xl" boxSize="full" color={color} />
         </Flex>
-      </CardBody>
-    </Card>
+      )}
+
+      {/* Chart Values */}
+      <Stat>
+        <StatLabel color="gray" fontWeight="light">
+          {title}
+        </StatLabel>
+
+        <StatNumber fontSize="xl" fontWeight="bold">
+          {value}
+        </StatNumber>
+
+        <StatHelpText>
+          <StatArrow type="decrease" />
+          9.05%
+        </StatHelpText>
+      </Stat>
+    </Flex>
   )
 }
