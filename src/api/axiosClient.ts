4215b9a0497ce357434e9bdc23a1c66d2b7a986e
Syrnnik <sakh.alexandr@gmail.com>
2025-01-18 11:28:42 +0300

    refactor: axios client

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosInstance } from "axios"
+import { getDeviceToken } from "util/authorizedDevice"
 import { getApiBaseUrl } from "util/urls"
-import { clearUserToken, getUserToken } from "util/userToken"
+import { getUserToken } from "util/userToken"
 
 export const axiosClient: AxiosInstance = axios.create({
   baseURL: getApiBaseUrl(),
@@ -8,9 +9,11 @@ export const axiosClient: AxiosInstance = axios.create({
 
 axiosClient.interceptors.request.use(async (config) => {
   const token = getUserToken()
+  const deviceToken = getDeviceToken()
 
   if (token) {
     config.headers["Authorization"] = `Bearer ${token}`
+    config.headers["device-token"] = `${deviceToken}`
   }
 
   return config
@@ -19,10 +22,17 @@ axiosClient.interceptors.request.use(async (config) => {
 axiosClient.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
-    if (error.response?.status === 401) {
-      clearUserToken()
+    const responseData = error.response?.data as { detail?: string }
+
+    if (responseData?.detail) {
+      // notify(responseData.detail, "error")
+      console.error(responseData.detail)
     }
 
+    // if (error.response?.status === 401) {
+    //   clearUserToken()
+    // }
+
     throw error
-  }
+  },
 )
