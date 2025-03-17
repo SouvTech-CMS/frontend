5c59dcf00a6070b1226e5ba988787d5901b191c8
Syrnnik <sakh.alexandr@gmail.com>
2025-03-17 03:40:44 +0300

    feat: redirect engraver if already processing order or if no order processing yet

--- src/context/engraving.tsx
+++ src/context/engraving.tsx
@@ -1,42 +1,46 @@
 import { getProcessingOrdersByEngraverId } from "api/engraver/processingOrder"
 import { ProcessingOrderStatus } from "constant/orderStatus"
 import { useUserContext } from "context/user"
-import { createContext, useContext } from "react"
+import { createContext, useContext, useEffect } from "react"
 import { useQuery } from "react-query"
+import { useLocation, useNavigate } from "react-router-dom"
 import { ProcessingOrder } from "type/engraver/processingOrder"
 import { FCC } from "type/fcc"
 import { WithId } from "type/withId"
 
 interface EngravingContextProps {
   currentProcessingOrder?: WithId<ProcessingOrder>
-  isCurrentProcessingOrderExists?: boolean
+  isCurrentProcessingOrderExists: boolean
   processingOrdersList?: WithId<ProcessingOrder>[]
   isProcessingOrdersListLoading: boolean
 }
 
-export const EngravingContext = createContext<EngravingContextProps>({
-  isProcessingOrdersListLoading: true,
-})
+export const EngravingContext = createContext<
+  EngravingContextProps | undefined
+>(undefined)
 
 export const EngravingContextProvider: FCC = (props) => {
   const { children } = props
 
-  const { currentEngraver } = useUserContext()
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const { currentEngraver, isLoadingCurrentUser } = useUserContext()
 
   const engraverId = currentEngraver?.id
 
   const {
     data: processingOrdersList,
     isLoading: isProcessingOrdersListLoading,
+    isRefetching,
+    refetch,
   } = useQuery<WithId<ProcessingOrder>[]>(
     ["processingOrdersList", engraverId],
     () => getProcessingOrdersByEngraverId(engraverId!),
-    {
-      enabled: !!engraverId,
-    },
   )
 
-  const isLoading = isProcessingOrdersListLoading
+  const isLoading =
+    isProcessingOrdersListLoading || isLoadingCurrentUser || isRefetching
 
   const currentProcessingOrder = processingOrdersList?.find(
     (processingOrder) =>
@@ -44,13 +48,43 @@ export const EngravingContextProvider: FCC = (props) => {
   )
   const isCurrentProcessingOrderExists = !!currentProcessingOrder
 
+  useEffect(() => {
+    if (!!engraverId) {
+      refetch()
+    }
+  }, [refetch, engraverId])
+
+  useEffect(() => {
+    if (isLoading) {
+      return
+    }
+
+    // Redirect engraver to already processing order
+    if (isCurrentProcessingOrderExists) {
+      const processingOrderId = currentProcessingOrder?.id
+      if (!processingOrderId) {
+        return
+      }
+
+      navigate(`/engraving/${processingOrderId}`, {
+        state: { from: location },
+      })
+    }
+    // Redirect engraver to "find order for engraving" page
+    else {
+      navigate("/engraving", {
+        state: { from: location },
+      })
+    }
+  }, [currentProcessingOrder])
+
   return (
     <EngravingContext.Provider
       value={{
         currentProcessingOrder,
         isCurrentProcessingOrderExists,
         processingOrdersList,
-        isProcessingOrdersListLoading: isLoading,
+        isProcessingOrdersListLoading,
       }}
     >
       {children}
