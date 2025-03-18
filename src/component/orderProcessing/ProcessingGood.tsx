48c716d2adb443d6949f928a5a6e538a1a829f97
Syrnnik <sakh.alexandr@gmail.com>
2025-03-18 22:08:37 +0300

    feat: link to good listing

--- src/component/orderProcessing/ProcessingGood.tsx
+++ src/component/orderProcessing/ProcessingGood.tsx
@@ -1,5 +1,6 @@
 import { Divider, Flex, Text } from "@chakra-ui/react"
 import { SKUBadge } from "component/badge/SKUBadge"
+import { MarketplaceGoodListingLink } from "component/good/MarketplaceGoodListingLink"
 import { ProcessingStorageGood } from "component/orderProcessing/ProcessingStorageGood"
 import { ToggleContainer } from "component/ToggleContainer"
 import { FC } from "react"
@@ -11,12 +12,6 @@ interface ProcessingGoodProps {
   goodsList: WithId<GoodWithDetailedStorageGoods>[]
   goodDetails: WithId<GoodDetails>
 }
-
-const PARAMS_TO_EXCLUDE_FROM_ENGRAVING_INFO = [
-  "listing_id",
-  "product_id",
-  "transaction_type",
-]
 
 export const ProcessingGood: FC<ProcessingGoodProps> = (props) => {
   const { goodsList, goodDetails } = props
@@ -30,9 +25,8 @@ export const ProcessingGood: FC<ProcessingGoodProps> = (props) => {
 
   const { storage_goods, ...good } = foundGood
 
-  const engravingInfo = parseEngravingInfoStr(
+  const { engravingInfo, goodListingParams } = parseEngravingInfoStr(
     goodDetails.engraving_info,
-    PARAMS_TO_EXCLUDE_FROM_ENGRAVING_INFO,
   )
 
   return (
@@ -42,7 +36,7 @@ export const ProcessingGood: FC<ProcessingGoodProps> = (props) => {
       <Flex w="full" direction="column" gap={5}>
         {/* Engraving Info */}
         <Flex w="full" direction="column" gap={2}>
-          {engravingInfo.map(([key, value], index) => (
+          {engravingInfo?.map(([key, value], index) => (
             <Flex key={index} w="full" direction="column">
               <Flex
                 key={index}
@@ -62,11 +56,20 @@ export const ProcessingGood: FC<ProcessingGoodProps> = (props) => {
 
         {/* Good Name & SKU */}
         <Flex w="full" direction="column" gap={2}>
-          {/* Good SKU & Name */}
-          <Flex w="full" direction="row" alignItems="center" gap={2}>
-            <SKUBadge sku={good.uniquename} size="lg" />
-            <Text>{good.name}</Text>
-          </Flex>
+          {/* Good SKU & Name with Link to Good Listing */}
+          <MarketplaceGoodListingLink goodListingParams={goodListingParams}>
+            <Flex
+              w="full"
+              direction="row"
+              alignItems="center"
+              flexWrap="wrap"
+              gap={2}
+            >
+              <SKUBadge sku={good.uniquename} size="lg" />
+
+              <Text>{good.name}</Text>
+            </Flex>
+          </MarketplaceGoodListingLink>
 
           {/* Storage Goods Toggle */}
           <ToggleContainer
--- src/util/engravingInfo.ts
+++ src/util/engravingInfo.ts
@@ -1,19 +1,42 @@
-export const parseEngravingInfoStr = (
-  engravingInfoStr?: string,
-  paramsToExclude?: string[],
+import { GoodListingParams } from "type/engraver/engravingInfo"
+
+const PARAMS_TO_EXCLUDE_FROM_ENGRAVING_INFO = [
+  "listing_id",
+  "product_id",
+  "transaction_type",
+]
+
+export const getEtsyGoodListingUrl = (
+  listingId?: number,
+  productId?: number,
 ) => {
-  if (!engravingInfoStr) {
-    return []
+  if (!listingId || !productId) {
+    return
   }
 
-  // Convert JSON-string to string
+  const url = `https://www.etsy.com/listing/${listingId}?transaction_id=${productId}`
+  return url
+}
+
+export const parseEngravingInfoStr = (engravingInfoStr?: string) => {
+  if (!engravingInfoStr) {
+    return { engravingInfo: undefined, goodListingParams: undefined }
+  }
+
+  // Convert JSON-string to object
   const engravingInfoJSON = JSON.parse(engravingInfoStr)
   const engravingInfo = Object.entries<string>(engravingInfoJSON)
 
   // Exclude params from Engraving Info
-  if (paramsToExclude?.length) {
-    return engravingInfo.filter(([key]) => !paramsToExclude.includes(key))
-  }
+  const filteredEngravingInfo = engravingInfo.filter(
+    ([key]) => !PARAMS_TO_EXCLUDE_FROM_ENGRAVING_INFO.includes(key),
+  )
 
-  return engravingInfo
+  const goodListingParams: GoodListingParams = Object.fromEntries(
+    engravingInfo.filter(([key]) =>
+      PARAMS_TO_EXCLUDE_FROM_ENGRAVING_INFO.includes(key),
+    ),
+  ) as GoodListingParams
+
+  return { engravingInfo: filteredEngravingInfo, goodListingParams }
 }
