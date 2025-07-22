80e9d0529e18fad36ac908f45f016e34e2691915
Syrnnik <sakh.alexandr@gmail.com>
2025-07-22 11:06:13 +0300

    fix: engraving page content component

--- src/component/orderProcessing/EngravingPanel.tsx
+++ src/component/orderProcessing/EngravingPanel.tsx
@@ -1,44 +1,27 @@
 import { Flex } from "@chakra-ui/react"
-import { Container } from "component/Container"
-import { ProcessingOrderButtons } from "component/orderProcessing/ProcessingOrderButtons"
-import { TicketChat } from "component/ticket/chat/TicketChat"
+import { EngravingChatAndButtons } from "component/orderProcessing/EngravingChatAndButtons"
+import { ProcessingOrderDetails } from "component/orderProcessing/ProcessingOrderDetails"
 import { FC } from "react"
+import { ProcessingOrder } from "type/engraver/processingOrder"
+import { WithId } from "type/withId"
 
 interface EngravingPanelProps {
+  processingOrder: WithId<ProcessingOrder>
   isOrderHasTicket?: boolean
   isViewOnlyMode?: boolean
 }
 
 export const EngravingPanel: FC<EngravingPanelProps> = (props) => {
-  const { isOrderHasTicket, isViewOnlyMode } = props
-
-  if (isViewOnlyMode && !isOrderHasTicket) {
-    return <></>
-  }
+  const { processingOrder, isOrderHasTicket, isViewOnlyMode } = props
 
   return (
-    <Flex h="full" flex={1} direction="column" gap={2}>
-      {!isViewOnlyMode && <Buttons isOrderHasTicket={isOrderHasTicket} />}
+    <Flex h="full" w="full" direction="row" overflow="hidden" gap={5}>
+      <ProcessingOrderDetails processingOrder={processingOrder} />
 
-      {isOrderHasTicket && <Chat />}
+      <EngravingChatAndButtons
+        isOrderHasTicket={isOrderHasTicket}
+        isViewOnlyMode={isViewOnlyMode}
+      />
     </Flex>
-  )
-}
-
-export const Buttons: FC<{ isOrderHasTicket?: boolean }> = (props) => {
-  const { isOrderHasTicket } = props
-
-  return (
-    <Container h={isOrderHasTicket ? undefined : "full"}>
-      <ProcessingOrderButtons />
-    </Container>
-  )
-}
-
-export const Chat: FC = () => {
-  return (
-    <Container h="full" bgColor="gray.200" p={0} overflowY="auto">
-      <TicketChat isReadOnly />
-    </Container>
   )
 }
