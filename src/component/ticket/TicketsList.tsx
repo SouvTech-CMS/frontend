72185240af34f7bb4eef674f6c053f7b66efecb7
Syrnnik <sakh.alexandr@gmail.com>
2026-09-14 21:06:48 +0300

    feat: tickets pagination and search by order id on backend

--- src/component/ticket/TicketsList.tsx
+++ src/component/ticket/TicketsList.tsx
@@ -1,17 +1,33 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Spinner, Text } from "@chakra-ui/react"
 import { TicketListItem } from "component/ticket/TicketListItem"
-import { FC } from "react"
+import { FC, UIEvent } from "react"
 import { FullTicket } from "type/ticket/ticket"
 import { WithId } from "type/withId"
 
+// NOTE: scrollTop can be fractional (browser zoom), so compare with a threshold
+const LOAD_MORE_SCROLL_THRESHOLD_PX = 50
+
 interface TicketsListProps {
   ticketsList?: WithId<FullTicket>[]
+  hasNextPage?: boolean
+  isFetchingNextPage?: boolean
+  fetchNextPage?: () => void
 }
 
 export const TicketsList: FC<TicketsListProps> = (props) => {
-  const { ticketsList } = props
+  const { ticketsList, hasNextPage, isFetchingNextPage, fetchNextPage } = props
 
   const isTicketsExist = !!ticketsList?.length
+
+  const handleScroll = (e: UIEvent<HTMLDivElement>) => {
+    const { scrollHeight, scrollTop, clientHeight } = e.currentTarget
+    const isAtBottom =
+      scrollHeight - scrollTop - clientHeight < LOAD_MORE_SCROLL_THRESHOLD_PX
+
+    if (isAtBottom && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage?.()
+    }
+  }
 
   return (
     <Flex
@@ -34,10 +50,21 @@ export const TicketsList: FC<TicketsListProps> = (props) => {
         </Flex>
       )}
 
-      <Flex w="full" direction="column" overflowY="auto">
-        {ticketsList?.map((ticket, index) => (
-          <TicketListItem key={index} fullTicket={ticket} />
+      <Flex
+        w="full"
+        direction="column"
+        overflowY="auto"
+        onScroll={handleScroll}
+      >
+        {ticketsList?.map((ticket) => (
+          <TicketListItem key={ticket.id} fullTicket={ticket} />
         ))}
+
+        {isFetchingNextPage && (
+          <Flex w="full" justifyContent="center" py={2}>
+            <Spinner color="red.400" size="sm" />
+          </Flex>
+        )}
       </Flex>
     </Flex>
   )
